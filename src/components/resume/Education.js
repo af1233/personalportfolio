fc4ec3ce1895f7968fda871fc6a871ea027265e5
Aashiq Farid <146064739+af1233@users.noreply.github.com>
2024-01-16 23:24:45 +0500

    Add files via upload

---
+++ src/components/resume/Education.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+import ResumeCard from './ResumeCard';
+
+const Education = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.5 } }}
+      className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20"
+    >
+      {/* part one */}
+      <div>
+        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]">2006 - 2023</p>
+          <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
+        </div>
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="BSc in Computer Science"
+            subTitle="University of Okara (2019 - 2023)"
+            result="3.4/4"
+            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
+          />
+          
+        </div>
+      </div>
+      {/* part Two */}
+
+      <div>
+        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]">2022 - 2024</p>
+          <h2 className="text-3xl md:text-4xl font-bold">Work Experience</h2>
+        </div>
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        
+          <ResumeCard
+            title="Front-end Developer"
+            subTitle="As FreeLancer - (2022 - 2023)"
+
+            des="Frontend developer proficient in HTML, CSS, and JavaScript, with a keen eye for design and user experience and optimize user interfaces for user interactions"
+          />
+        </div>
+      </div>
+    </motion.div>
+  );
+}
+
+export default Education
---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe} from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
+
+// Features Data
+export const featuresData = [
+  {
+    id: 1,
+    title: "Frontend",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 2,
+    icon: <AiFillAppstore />,
+    title: "Web App Development",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 3,
+    icon: <SiProgress />,
+    title: "Mern Stack Development",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 4,
+    icon: <SiAntdesign />,
+    title: "Hosting Websites",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  
+];
